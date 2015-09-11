e3cb97b5f7975a22659c0aa5c51ceddab2277be3
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-09-11 15:16:49 -0300

    Create images task

--- tasks/config.js
+++ tasks/config.js
@@ -6,13 +6,23 @@ module.exports = {
 		path: dest
 	},
 
+	images: {
+		src: src + '/assets/img/**'
+		dest: dest + '/assets/img',
+		settings: {
+			optimizationLevel: 3,
+			progressive: true,
+			interlaced: true
+		}
+	},
+
 	server: {
 		baseDir: dest,
 		port: 4000
 	},
-	
+
 	stylus: {
 		src: src + '/styl/**/*.styl',
 		dest: dest + '/assets/css'
-	},
+	}
 };
